e53049ad5467a392f51c9bed311fb5ddd31a2747
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-05-12 14:48:34 +0600

    Added Utils

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                // Search for existing instance.
+                instance = (T) FindObjectOfType(typeof(T));
+
+                // Create new instance if one doesn't already exist.
+                if (instance == null)
+                {
+                    // Need to create a new GameObject to attach the singleton to.
+                    var singletonObject = new GameObject();
+                    instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+
+                    // Make instance persistent.
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+
+            return instance;
+        }
+    }
+}
