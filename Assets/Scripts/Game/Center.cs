fed671726628d2c61e687ded980463a2dd618ff9
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-05-14 06:58:21 +0600

    Center on click

--- Assets/Scripts/Game/Center.cs
+++ Assets/Scripts/Game/Center.cs
@@ -9,15 +9,39 @@ public class Center : MonoBehaviour
         public const float SMALL = 1.2f;
     }
 
-    [SerializeField] private GameObject[] orbiters;
+    [SerializeField] private Orbit[] orbiters;
     
     void Start()
     {
-        
+        orbiters = FindObjectsOfType<Orbit>();
     }
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100.0f))
+            {
+                // TODO Compare Tag with planet
+                // TODO Lerp position
+                // TODO Lerp scale
+                // TODO Lerp alpha
+                hit.transform.position = Vector3.zero;
+                
+                foreach (var orbit in orbiters)
+                {
+                    if (orbit.transform.GetHashCode().Equals(hit.transform.GetHashCode()))
+                    {
+                        continue;
+                    }
+                    
+                    Color color = orbit.gameObject.GetComponent<Renderer>().material.color;
+                    color.a = 0;
+                    orbit.gameObject.GetComponent<Renderer>().material.color = color;
+                }
+            }
+        }
     }
 }
