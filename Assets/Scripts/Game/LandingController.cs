461ddc931694c2eea3cc27fb31cc74da94403605
Pahlwan Rabiul Islam <abirpahlwan@hotmail.com>
2020-05-14 10:57:37 +0600

    Launch rocket

---
+++ Assets/Scripts/Game/LandingController.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class LandingController : MonoBehaviour
+{
+    [SerializeField] private GameObject rocket;
+}
---
+++ Assets/Scripts/Game/Rocket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private float startTime;
+    private float journeyLength;
+    
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(transform.position, Vector3.zero);
+    }
+    
+    void Update()
+    {
+        float distCovered = (Time.time - startTime);
+        float fractionOfJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(transform.position, Vector3.zero, fractionOfJourney);
+    }
+}
